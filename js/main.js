ac92f3a4cadb7c443d2629e215c2ea2e226a4e34
nefro <mariusz.sygnowski@gmail.com>
2016-01-17 13:09:21 +0100

    list selection is working

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 var SiteNav = React.createClass({
-
     getInitialState: function () {
         return {selectedNav: []};
     },
-
-
     handelSelect: function (id) {
-        console.log('handle select ' + id);
+        this.setState({selectedNav: [id]})
     },
-
-
     render: function () {
         var hnd = this.handelSelect;
-
+        var selection = this.state.selectedNav;
         var navItems = this.props.data.map(function (link) {
-            return <SiteNavItem onHandleSelection={hnd} key={link.id} navInfo={link}/>
+
+            var isSelected = -1 != selection.indexOf(String(link.id));
+
+            return <SiteNavItem onHandleSelection={hnd} key={link.id} navInfo={link} isSelected={isSelected}/>
         });
 
         return (
@@ -26,19 +24,21 @@ var SiteNav = React.createClass({
 });
 
 var SiteNavItem = React.createClass({
-
     getInitialState: function () {
         return {isSelected: false};
     },
-
     handleClick: function (event) {
         this.props.onHandleSelection(event.target.id);
     },
-
     render: function () {
+        var css = "list-group-item";
+        if (this.props.isSelected) {
+            css += " active";
+        }
+
         return <a id={this.props.navInfo.id}
                   onClick={this.handleClick}
-                  href={this.props.navInfo.link} className="list-group-item">{this.props.navInfo.title}</a>;
+                  href={this.props.navInfo.link} className={css}>{this.props.navInfo.title}</a>;
     }
 });
 
